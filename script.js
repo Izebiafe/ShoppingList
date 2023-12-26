6ae55607e425620769b1710e3d38135e88fca73f
izebiafe <afimiafidelis95@gmail.com>
2023-12-26 10:09:11 +0100

    add clear and remove item

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 function addItem(e) {
   e.preventDefault();
@@ -33,4 +34,22 @@ function createIcon(classes) {
   return icon;
 }
 
+function removeItem(e) {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+function clearItems(e) {
+  if (e.target.id === 'clear') {
+    if (confirm('Are you sure?')) {
+      itemList.innerHTML = '';
+    }
+  }
+}
+
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
